b4023cfcb57b1ebc9e1789e45d78f4b860edb1d1
Fariza Kubanova <farizatkubanova@gmail.com>
2022-09-10 21:06:36 +0100

    добавила разные стили карточкам

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Tarif from './Components/Tarif';
 
 
 const tarifs=[
-  {title:'Безлимитный 300', price:'300/мес', speed: 'до 10 Мбит/сек', text:'объем включенного трафика не ограничен'},
-  {title:'Безлимитный 450', price:'450/мес', speed: 'до 50 Мбит/сек', text:'объем включенного трафика не ограничен'},
-  {title:'Безлимитный 550', price:'550/мес', speed: 'до 100 Мбит/сек', text:'объем включенного трафика не ограничен', isSelected: true},
-  {title:'Безлимитный 1000', price:'1000/мес', speed: 'до 200 Мбит/сек', text:'объем включенного трафика не ограничен'}
+  {title:'Безлимитный 300', price:'300/мес', speed: 'до 10 Мбит/сек', text:'объем включенного трафика не ограничен', background: 'blue'},
+  {title:'Безлимитный 450', price:'450/мес', speed: 'до 50 Мбит/сек', text:'объем включенного трафика не ограничен',  background: 'green'},
+  {title:'Безлимитный 550', price:'550/мес', speed: 'до 100 Мбит/сек', text:'объем включенного трафика не ограничен', background: 'yellow', isSelected: true},
+  {title:'Безлимитный 1000', price:'1000/мес', speed: 'до 200 Мбит/сек', text:'объем включенного трафика не ограничен', background: 'red'}
 ]
 
 
@@ -16,14 +16,15 @@ function App() {
       {
         tarifs.map((tarif, index)=>
 <Tarif 
-    key={index}  
+    key={index}
     title={tarif.title} 
     price={tarif.price} 
     speed={tarif.speed} 
-    text={tarif.text} 
-    isSelected={tarif.isSelected}/>
-        )
-      }
+    text={tarif.text}
+    isSelected={tarif.isSelected}
+    background={tarif.background}
+    />
+        )}
     </div>
   );
 }
--- src/Components/Tarif.jsx
+++ src/Components/Tarif.jsx
@@ -1,28 +1,34 @@
 import './tarif.css';
 import { useState } from 'react';
 
+
+
 function Tarif(props) {
-const {title, price, speed, text}=props;
+const {title, price, speed, text, background}=props;
 
 const [pressed, setPressed]= useState(false);
 const handleChange=()=>{
 setPressed(!pressed)
 }
 return (
-        <div onClick={handleChange} className={"card "+ (pressed?'selected':'')}>
-        <div className="cardTitle">
-{title}
-        </div>
-        <div className="cardPrice">
-{price}
+        
+        <div onClick={handleChange}  className={'card '  + (pressed?'selected':'')}>
+        <div className={background}>
+        <div className='cardTitle' >
+        {title}
+                </div>
+        <div className='cardPrice'>
+        {price}
+                </div>
         </div>
         <div className="cardSpeed">
-{speed}
-        </div>
+        {speed}
+                </div>
         <div className="cardText">
-{text}
+        {text}
+                </div>
+        
         </div>
-        </div>
-);
-}
+)}
+        
 export default Tarif;
